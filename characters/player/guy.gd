45f71d8be6e75324b2e36108dad9a600ec299da0
JasonDavidoff <jasondavidoff85@gmail.com>
2024-10-10 22:21:52 -0600

    fixed movement and file structure

--- characters/player/guy.gd
+++ characters/player/guy.gd
@@ -1,29 +1,28 @@
 extends CharacterBody3D
 
 
-const SPEED = 2.0
-const ROTATE_SPEED = 0.1
-const JUMP_VELOCITY = 4.5
+const SPEED = 1.0
+const ROTATE_SPEED = 0.05
 
 @onready var animation_tree: AnimationTree = $AnimationTree
 
-func _physics_process(_delta: float) -> void:
+func _physics_process(delta: float) -> void:
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	print(input_dir)
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		#look_at(transform.origin + direction)
 		animation_tree["parameters/StateMachine/conditions/is_moving"] = true
 		animation_tree["parameters/StateMachine/conditions/idle"] = false
-		velocity.z = -direction.z * SPEED
-		rotate_y(ROTATE_SPEED * input_dir[0])
+		velocity = transform.basis.z * SPEED * -input_dir[1]
+		rotate_y(ROTATE_SPEED * -input_dir[0])
 		
 	else:
 		animation_tree["parameters/StateMachine/conditions/is_moving"] = false
 		animation_tree["parameters/StateMachine/conditions/idle"] = true
 		velocity.z = move_toward(velocity.z, 0, SPEED)
+		velocity = Vector3(0,0,0)
 
 	move_and_slide()
